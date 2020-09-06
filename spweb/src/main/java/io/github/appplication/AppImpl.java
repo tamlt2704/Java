f1f524ab2c1a6c5e2246352631addd576f966ced
debian <debian@DESKTOP-Q0HSLBF.debian>
2020-09-06 07:50:32 +0000

    add npm plugin

--- spweb/src/main/java/io/github/appplication/AppImpl.java
+++ spweb/src/main/java/io/github/appplication/AppImpl.java
@@ -1,5 +1,6 @@
 package io.github.appplication;
 
+import io.github.appplication.model.Employee;
 import io.github.appplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,5 +18,11 @@ public class AppImpl implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Application running...");
+        this.employeeRepository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+        this.employeeRepository.save(new Employee("Gandalf", "the Grey", "wizard"));
+        this.employeeRepository.save(new Employee("Samwise", "Gamgee", "gardener"));
+        this.employeeRepository.save(new Employee("Meriadoc", "Brandybuck", "pony rider"));
+        this.employeeRepository.save(new Employee("Peregrin", "Took", "pipe smoker"));
     }
 }
--- spweb/src/main/java/io/github/appplication/repository/EmployeeRepository.java
+++ spweb/src/main/java/io/github/appplication/repository/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package io.github.appplication.repository;
 
 import io.github.appplication.model.Employee;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 }
