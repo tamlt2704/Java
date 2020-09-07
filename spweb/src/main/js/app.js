e9e8a5acc5dffe43622df35d8a793dd27541c98d
debian <debian@DESKTOP-Q0HSLBF.debian>
2020-09-07 14:39:09 +0000

    use axios

--- spweb/src/main/js/app.js
+++ spweb/src/main/js/app.js
@@ -2,22 +2,32 @@
 
 import React, {useState, useRef, useEffect} from 'react';
 const ReactDOM = require('react-dom');
-const client = require('./client');
+/*const client = require('./client');*/
 import props from 'prop-types';
 
 import EmployeeList from './components/EmployeeList';
 import Employee from './components/Employee';
-
+const axios = require('axios');
+const root = '/api';
 
 function App() {
 	const [employees, setEmployees] = useState([]);
+	const [pageSize, setPageSize] = useState(2);
+	const [pageNumber, setPageNumber] = useState(0);
+	
+	const loadFromServer = (pageSize) => {
+		axios({
+			method: 'get',
+			url: `${root}/employees?page=${pageNumber}&size=${pageSize}`			
+		})
+		.then((response) => {
+			console.log(response);
+			setEmployees(response?.data?._embedded?.employees);
+		});
+	}
 	
 	useEffect(() => {
-		client({method: 'GET', path: '/api/employees'})
-		.done(response => {
-			console.log(response);
-			setEmployees(response.entity._embedded.employees);
-		});
+		loadFromServer(pageSize);
 	}, [])
 	
 	return (
